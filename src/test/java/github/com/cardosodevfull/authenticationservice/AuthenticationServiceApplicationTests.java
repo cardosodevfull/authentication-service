bfbf11cac11e01738be9cb2f281cd8fe16f04a4c
Francisco Cardoso <cardosodevfull@gmail.com>
2023-09-05 15:14:03 -0300

    Setup inicial com conexão db postgresql

---
+++ src/test/java/github/com/cardosodevfull/authenticationservice/AuthenticationServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package github.com.cardosodevfull.authenticationservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AuthenticationServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
