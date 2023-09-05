53c2d800b044eb9cfd23da3cfd811aa84d7081f8
Francisco Cardoso <cardosodevfull@gmail.com>
2023-09-05 16:09:41 -0300

    Implementado o endpoint de registro de usuário

---
+++ src/main/java/github/com/cardosodevfull/authenticationservice/controller/UserController.java
@@ -0,0 +1,23 @@
+package github.com.cardosodevfull.authenticationservice.controller;
+
+import github.com.cardosodevfull.authenticationservice.dto.SignUpRequest;
+import github.com.cardosodevfull.authenticationservice.service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/auht/")
+public class UserController {
+    @Autowired
+    UserService userService;
+
+    @PostMapping("signUp")
+    public ResponseEntity<Object> signUp(@RequestBody SignUpRequest request) {
+        return ResponseEntity.status(HttpStatus.CREATED).body(userService.create(request));
+    }
+}
