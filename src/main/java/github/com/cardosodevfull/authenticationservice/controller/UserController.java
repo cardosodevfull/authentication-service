ab2c252b82fae7521842ed55754842db18d31d9a
Francisco Cardoso <cardosodevfull@gmail.com>
2023-09-05 16:21:12 -0300

    Correção doendereço endpoint signUp e testando registro de usuário no postmam

--- src/main/java/github/com/cardosodevfull/authenticationservice/controller/UserController.java
+++ src/main/java/github/com/cardosodevfull/authenticationservice/controller/UserController.java
@@ -11,7 +11,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("/auht/")
+@RequestMapping("/auth/")
 public class UserController {
     @Autowired
     UserService userService;
